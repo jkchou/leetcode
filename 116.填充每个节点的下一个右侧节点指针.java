4fd8788af9007a26d2ff626b60690e15f0c04aea
jkchou <jkchou0306@gmail.com>
2022-02-16 22:37:37 +0800

    2.16二叉树+深度/广度优先搜索

---
+++ 116.填充每个节点的下一个右侧节点指针.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=116 lang=java
+ *
+ * [116] 填充每个节点的下一个右侧节点指针
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        Node tempNode = new Node();
+        tempNode = root;
+        Node head = new Node();
+        head = root;
+        while(root!=null&&root.left != null){
+            root.left.next = root.right;
+            if(root.next != null){
+                root.right.next = root.next.left;
+                root = root.next;
+                continue;
+            } 
+            root = tempNode.left;
+            tempNode = root ;
+        }    
+        return head;
+    }
+}
+// @lc code=end
+
